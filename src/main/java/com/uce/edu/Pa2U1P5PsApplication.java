b1f48097772afd4101a60dd29a03f775bd522e57
Simbana_Pablo <pablo.simbana01@gmail.com>
2023-11-09 13:32:50 -0500

    segundo cambio en la misma rama

--- src/main/java/com/uce/edu/Pa2U1P5PsApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5PsApplication.java
@@ -15,6 +15,7 @@ public class Pa2U1P5PsApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println("Cambio en la rama");
 	
 	}
 
