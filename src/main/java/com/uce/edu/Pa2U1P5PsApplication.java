ad234d19318187e9798e97deb2f67dfa0bb7942f
pablo <pablo.simbana01@gmail.com>
2023-11-20 19:51:07 -0500

    taller8

---
+++ src/main/java/com/uce/edu/Pa2U1P5PsApplication.java
@@ -0,0 +1,52 @@
+package com.uce.edu;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.uce.edu.repository.modelo.Materia;
+import com.uce.edu.service.IMateriaService;
+
+@SpringBootApplication
+public class Pa2U1P5PsApplication implements CommandLineRunner{
+	
+	@Autowired
+	private Materia materia;
+	
+	@Autowired
+	private Materia materia1;
+	
+
+	@Autowired
+	private Materia materia2;
+
+	@Autowired
+	private IMateriaService iMateriaService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5PsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.materia.setNombre("Avanzada II");
+		System.out.println(this.materia);
+		
+		System.out.println(this.materia1);
+		this.materia.setNombre("nuevo nombre");
+		
+		System.out.println(this.materia);
+		System.out.println(this.materia1);
+		
+		this.materia2.setNombre("nombre final");
+		System.out.println(this.materia2);
+		System.out.println(this.materia1);
+		System.out.println(this.materia);
+		
+		
+		
+	}
+}
---
+++ src/main/java/com/uce/edu/repository/MateriaRepositoryImp.java
@@ -0,0 +1,51 @@
+package com.uce.edu.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.uce.edu.repository.modelo.Materia;
+@Repository
+public class MateriaRepositoryImp implements IMateriaRepository{
+	
+	public static List<Materia> base = new ArrayList<Materia>();
+	
+	@Override
+	public Materia seleccionar(String codigo) {
+		System.out.println("Se selecciono el codigo: " +codigo);
+		for(Materia ma: base) {
+			if(ma.getCodigo().equals(codigo))
+				return ma;
+			return null;
+		}
+		return new Materia();
+	}
+
+	@Override
+	public void insertar(Materia materia) {
+		base.add(materia);
+		System.out.println("Se inserto: "+materia);
+	}
+
+	@Override
+	public void actualizar(Materia materia) {
+		this.borrar(materia.getCodigo());
+		this.insertar(materia);
+		System.out.println("Se actualizo : "+materia);
+	}
+
+	@Override
+	public void borrar(String codigo) {
+		Materia materia = this.seleccionar(codigo);
+		base.remove(materia);
+		System.out.println("Se eliminio: "+codigo);
+	}
+
+	@Override
+	public List<Materia> seleccionarTodos() {
+		System.out.println("Reporte inicio");
+		return base;
+	}
+
+}
